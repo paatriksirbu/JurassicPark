70658938b8caa6c03fb64ce464e9587cbdc4d698
Patrik <psirb@myuax.com>
2024-11-06 14:46:19 +0100

    Metodo generico para aplicar backpressure en cualquier flujo de datos cuando se necesite.

--- src/main/java/com/gapplabs/jurasicpark/config/BackpressureConfig.java
+++ src/main/java/com/gapplabs/jurasicpark/config/BackpressureConfig.java
@@ -1,4 +1,15 @@
 package com.gapplabs.jurasicpark.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.scheduler.Schedulers;
+
+@Configuration
 public class BackpressureConfig {
+
+    @Bean
+    public Schedulers defaultSchedulers() {
+        // Configuramos el Schedulers por defecto para que sea el boundedElastic y manejar la concurrencia.
+        return (Schedulers) Schedulers.boundedElastic();
+    }
 }
