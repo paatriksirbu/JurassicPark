d9be588c44e9497143444bfe82c7f11db7d76941
paatriksirbu <psirb@myuax.com>
2024-11-06 19:43:28 +0100

    Clase SensorData con atributos, constructores y getters y setters definidos.

--- src/main/java/com/gapplabs/jurasicpark/models/SensorData.java
+++ src/main/java/com/gapplabs/jurasicpark/models/SensorData.java
@@ -1,4 +1,24 @@
 package com.gapplabs.jurasicpark.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class SensorData {
+    private String id;
+    private String sensorId;
+    private Double value;
+    private Long timestamp;
+
+    // Constructor vacio
+    public SensorData() {}
+
+    // Constructor con todos los atributos
+    public SensorData(String id, String sensorId, Double value, Long timestamp) {
+        this.id = id;
+        this.sensorId = sensorId;
+        this.value = value;
+        this.timestamp = timestamp;
+    }
 }
