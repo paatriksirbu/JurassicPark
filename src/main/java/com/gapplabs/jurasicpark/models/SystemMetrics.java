7bf0ccadfe70fca206743572263387d9120d973e
paatriksirbu <psirb@myuax.com>
2024-11-06 19:46:44 +0100

    Clase SystemMetrics con atributos, constructores y getters y setters definidos.

--- src/main/java/com/gapplabs/jurasicpark/models/SystemMetrics.java
+++ src/main/java/com/gapplabs/jurasicpark/models/SystemMetrics.java
@@ -1,4 +1,22 @@
 package com.gapplabs.jurasicpark.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class SystemMetrics {
+    private String metricName;
+    private Double value;
+    private Long timestamp;
+
+    // Constructor vacio. Lo creamos para que el compilador no nos de errores en caso de que no lo usemos
+    public SystemMetrics() {}
+
+    // Constructor con todos los atributos
+    public SystemMetrics(String metricName, Double value, Long timestamp) {
+        this.metricName = metricName;
+        this.value = value;
+        this.timestamp = timestamp;
+    }
 }
