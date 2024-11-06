b9a647f9d6a61a75016c2703004a6bce311035a8
paatriksirbu <psirb@myuax.com>
2024-11-06 19:23:16 +0100

    Configuracion del firebase.

--- src/main/java/com/gapplabs/jurasicpark/config/FirebaseConfig.java
+++ src/main/java/com/gapplabs/jurasicpark/config/FirebaseConfig.java
@@ -1,6 +1,31 @@
 package com.gapplabs.jurasicpark.config;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
 public class FirebaseConfig {
 
+    @Bean
+    public FirebaseApp initializeFirebase() throws IOException {
+        FileInputStream serviceAccount = new FileInputStream("src/main/resources/jurassicpark-firebase-service-account.json");
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+
+        if (FirebaseApp.getApps().isEmpty()){
+            FirebaseApp.initializeApp(options);
+        }
+
+     return FirebaseApp.getInstance();
+    }
 
 }
