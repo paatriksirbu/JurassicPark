bf466e5d7a5e9a6c7659b9a5d82537548d369f82
Patrik <psirb@myuax.com>
2024-11-06 14:52:25 +0100

    Arreglos. Definimos todos los aspectos que tenemos creados.

--- src/main/java/com/gapplabs/jurasicpark/aspects/LoggingAspect.java
+++ src/main/java/com/gapplabs/jurasicpark/aspects/LoggingAspect.java
@@ -1,4 +1,4 @@
 package com.gapplabs.jurasicpark.aspects;
 
-public class LogginAspect {
+public class LoggingAspect {
 }
--- src/main/java/com/gapplabs/jurasicpark/config/AspectConfig.java
+++ src/main/java/com/gapplabs/jurasicpark/config/AspectConfig.java
@@ -1,4 +1,32 @@
 package com.gapplabs.jurasicpark.config;
 
+import com.gapplabs.jurasicpark.aspects.LoggingAspect;
+import com.gapplabs.jurasicpark.aspects.MonitoringAspect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy // Habilitamos el uso de aspectos
 public class AspectConfig {
+
+    @Bean
+    public LoggingAspect loggingAspect() {
+        return new LoggingAspect();
+    }
+
+    @Bean
+    public BackpressureConfig backpressureConfig() {
+        return new BackpressureConfig();
+    }
+
+    @Bean
+    public SecurityConfig securityConfig() {
+        return new SecurityConfig();
+    }
+
+    @Bean
+    public MonitoringAspect monitoringAspect() {
+        return new MonitoringAspect();
+    }
 }
