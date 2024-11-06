de9ff27c8674a93c786fdfd72a918c569c55b263
Patrik <psirb@myuax.com>
2024-11-06 14:36:29 +0100

    Definimos un ejecutor de tareas para manejar tareas en segundo plano

--- src/main/java/com/gapplabs/jurasicpark/config/WebFluxConfig.java
+++ src/main/java/com/gapplabs/jurasicpark/config/WebFluxConfig.java
@@ -1,4 +1,22 @@
 package com.gapplabs.jurasicpark.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+@Configuration
+@EnableWebFlux
 public class WebFluxConfig {
+
+    @Bean
+    public ThreadPoolTaskExecutor taskExecutor(){
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(50);
+        executor.setQueueCapacity(200);
+        executor.setThreadNamePrefix("JurassicTaskExecutor-");
+        executor.initialize();
+        return executor;
+    }
 }
