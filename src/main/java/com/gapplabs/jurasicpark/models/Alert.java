1310326a6a2b4ee4392396d5abd32a8fae68b2b4
paatriksirbu <psirb@myuax.com>
2024-11-06 19:44:13 +0100

    Clase Alert con atributos, constructores y getters y setters definidos.

--- src/main/java/com/gapplabs/jurasicpark/models/Alert.java
+++ src/main/java/com/gapplabs/jurasicpark/models/Alert.java
@@ -1,4 +1,25 @@
 package com.gapplabs.jurasicpark.models;
 
+import com.gapplabs.jurasicpark.enums.AlertLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Alert {
+    private String id;
+    private AlertLevel level;
+    private String description;
+    private Long timestamp;
+
+    // Constructor vacio
+    public Alert() {}
+
+    // Constructor con todos los atributos
+    public Alert(String id, AlertLevel level, String description, Long timestamp) {
+        this.id = id;
+        this.level = level;
+        this.description = description;
+        this.timestamp = timestamp;
+    }
 }
