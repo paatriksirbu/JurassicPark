eba27c5578ee830536ebda5a9c3a8f2aad717a1d
Patrik <psirb@myuax.com>
2024-11-06 11:25:11 +0100

    Initial commit

---
+++ src/main/java/com/gapplabs/jurasicpark/JurasicParkApplication.java
@@ -0,0 +1,13 @@
+package com.gapplabs.jurasicpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JurasicParkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JurasicParkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gapplabs/jurasicpark/JurasicParkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gapplabs.jurasicpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JurasicParkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
