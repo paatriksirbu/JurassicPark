beab63b3d8dac914376334d0ebe332fad85cd5ec
paatriksirbu <psirb@myuax.com>
2024-11-06 19:42:30 +0100

    Clase sensor con atributos, constructores y getters y setters definidos.

--- src/main/java/com/gapplabs/jurasicpark/models/Sensor.java
+++ src/main/java/com/gapplabs/jurasicpark/models/Sensor.java
@@ -1,4 +1,26 @@
 package com.gapplabs.jurasicpark.models;
 
+import com.gapplabs.jurasicpark.enums.SensorStatus;
+import com.gapplabs.jurasicpark.enums.SensorType;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Sensor {
+    private String id;
+    private SensorType type;
+    private SensorStatus status;
+    private String location;
+
+    // Constructor vacio
+    public Sensor() {}
+
+    // Constructor con todos los atributos
+    public Sensor(String id, SensorType type, SensorStatus status, String location) {
+        this.id = id;
+        this.type = type;
+        this.status = status;
+        this.location = location;
+    }
 }
