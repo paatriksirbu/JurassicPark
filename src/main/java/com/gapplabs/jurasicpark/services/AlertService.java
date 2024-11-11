ff5dfa5b822e5f96c5fd331c3c4bfd2616f7a325
paatriksirbu <psirb@myuax.com>
2024-11-11 12:21:55 +0100

    Declaracion de variables y constructor del AlertService.

--- src/main/java/com/gapplabs/jurasicpark/services/AlertService.java
+++ src/main/java/com/gapplabs/jurasicpark/services/AlertService.java
@@ -1,4 +1,20 @@
 package com.gapplabs.jurasicpark.services;
 
+import com.gapplabs.jurasicpark.models.Alert;
+import com.gapplabs.jurasicpark.repositories.AlertRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AlertService {
+
+    private static final Logger logger = LoggerFactory.getLogger(AlertService.class);
+    private final AlertRepository alertRepository;
+
+    public AlertService(AlertRepository alertRepository) {
+        this.alertRepository = alertRepository;
+    }
+
+
 }
