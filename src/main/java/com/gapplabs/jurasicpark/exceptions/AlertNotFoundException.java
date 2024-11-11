28f521cd34253c0adca539fd8b3ae5bcd619d857
paatriksirbu <psirb@myuax.com>
2024-11-11 12:20:27 +0100

    Excepcion que se lanzara cuando no se encuentre una alerta especifica en la base de datos.

---
+++ src/main/java/com/gapplabs/jurasicpark/exceptions/AlertNotFoundException.java
@@ -0,0 +1,7 @@
+package com.gapplabs.jurasicpark.exceptions;
+
+public class AlertNotFoundException extends RuntimeException {
+    public AlertNotFoundException(String message) {
+        super(message);
+    }
+}
