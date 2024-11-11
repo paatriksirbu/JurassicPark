99ffebb62011b90193ba6ea67f2a8eef5f663943
paatriksirbu <psirb@myuax.com>
2024-11-11 12:24:17 +0100

    Metodo para guardar alertas en AlertService.

--- src/main/java/com/gapplabs/jurasicpark/services/AlertService.java
+++ src/main/java/com/gapplabs/jurasicpark/services/AlertService.java
@@ -2,9 +2,13 @@ package com.gapplabs.jurasicpark.services;
 
 import com.gapplabs.jurasicpark.models.Alert;
 import com.gapplabs.jurasicpark.repositories.AlertRepository;
+import com.google.cloud.firestore.WriteResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 @Service
 public class AlertService {
@@ -16,5 +20,17 @@ public class AlertService {
         this.alertRepository = alertRepository;
     }
 
+    public CompletableFuture<String> saveAlert(Alert alert) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                WriteResult result = alertRepository.saveAlert(alert).get();
+                logger.info("Alerta guardada correctamente: " + result);
+                return result.getUpdateTime().toString();
+            } catch (InterruptedException | ExecutionException e) {
+                logger.error("Error al guardar la alerta: " + e.getMessage());
+                throw new RuntimeException("Error al guardar la alerta: " + e.getMessage());
+            }
+        });
+    }
 
 }
